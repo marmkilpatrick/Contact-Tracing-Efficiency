98caa2188142180567c156c2443e7bd7a641827b
marmkilpatrick <marmkilpatrick@gmail.com>
2021-06-05 21:25:01 -0700

    Add files via upload

---
+++ Fig 1_2_S3 Final.r
@@ -0,0 +1,227 @@
+setwd("c:/marm/research/covid-19/modeling T-CT-I/")
+lapply(c("ggplot2","tidyverse","deSolve"),require,character.only=T) #load packages
+
+R0a = function(parsR) { #function for R0 expression
+  with(parsR,
+       kappa*beta*(S/N)*(sigma_Is*q_Ips_Is/(alpha+gamma_Is+tau_Is+eps_Is_Ips*ftraced+eps_Is_Is*ftraced)*(q_E_Ips/(q_Ips_Is+eps_Ips_Ips*ftraced+eps_Ips_Is*ftraced))
+                         +(sigma_Ips*q_E_Ips/(q_Ips_Is+eps_Ips_Ips*ftraced+eps_Ips_Is*ftraced)))*(1/(q_E_Ips+eps_E_Ips*ftraced+eps_E_Is*ftraced))) }
+#params
+#Infectiousness vs day from He et al gamma distribution parameterized w/ mean, variance
+shape=function(mu,sig) mu^2/sig^2;scale = function(mu,sig) sig^2/mu;mn=3.1;sig2=2.15
+presympInf=pgamma(2.3,shape=shape(mn,sig2),scale=scale(mn,sig2))/2.3 #relative daily infectiousness pre-symptomatic
+postsympInf=(pgamma(7.3,shape=shape(mn,sig2),scale=scale(mn,sig2))-pgamma(2.3,shape=shape(mn,sig2),scale=scale(mn,sig2)))/5 #relative daily infectiousness mildly-symptomatic
+ssInf=(pgamma(13.3,shape=shape(mn,sig2),scale=scale(mn,sig2))-pgamma(7.3,shape=shape(mn,sig2),scale=scale(mn,sig2)))/6 #relative daily infectiousness severely-symptomatic
+
+Ninf=c(10^seq(0.2,3.4,by=.01)) # # of cases/infections traced
+#Ninf=100 # # of cases/infections traced
+Ncont_p_inf= 10 #c(5,10,20,30) # contacts per infection - value before lockdowns 30
+NCCTD=12 # # of contacts a contact tracer can contact per day to isolate; 1.5/hr
+NCT=15 # # of contact tracers for population - CA target
+capcontratio=(NCT*NCCTD)/(Ninf*Ncont_p_inf);capcontratio[capcontratio>1]=1
+tdelay=c(1:5,10);testdelay=rep(tdelay,each=length(Ninf))
+#tdelay=5;testdelay=rep(tdelay,each=length(Ninf))
+
+pars1 = data.frame(N=100001,S=100001,
+                   beta = 0.35,
+                   kappa=1,
+                   sigma_Ips=presympInf/postsympInf,
+                   sigma_Is=1,
+                   gamma_Is=1/5,
+                   q_E_Ips=1/3.2,
+                   q_Ips_Is=1/2.3,
+                   q_Q_R=1/14,
+                   tau_Is=1/testdelay,
+                   ftraced=capcontratio,
+                   tracetime=0.5,
+                   CTR=T,
+                   Ncont_p_inf= 10, # contacts per infection
+                   NCCTD=12, # # of contacts a contact tracer can contact per day to isolate; 1.5/hr
+                   NCT=15 # # of contact tracers for population
+)
+
+pars1 = pars1 %>% mutate (
+  alpha=gamma_Is*(0.0066/0.8)/(1-(0.0066/0.8)),
+  f_test=tau_Is/(tau_Is+gamma_Is),
+  f_Ips=(sigma_Ips/q_Ips_Is)/
+    (sigma_Ips/q_Ips_Is+sigma_Is/(gamma_Is+tau_Is)),
+  f_Is=(sigma_Is/(gamma_Is+tau_Is))/
+    (sigma_Ips/q_Ips_Is+sigma_Is/(gamma_Is+tau_Is)),
+  ttt_Ips=1/tau_Is+1/q_Ips_Is+tracetime, #time to trace infect caused by Ips
+  ttt_Is=1/tau_Is+tracetime, #time to trace infect caused by Ips
+  f_E_Ips=exp(-q_E_Ips*ttt_Ips), #fraction of traced infections caused while Ips still in Ea
+  f_E_Is=exp(-q_E_Ips*ttt_Is), #fraction of traced infections caused while Is still in Ea
+  f_Ips_Ips=(1-f_E_Ips)*exp(-(1/(1/q_E_Ips+1/q_Ips_Is)*ttt_Ips)),
+  f_Ips_Is=(1-f_E_Is)*exp(-(1/(1/q_E_Ips+1/q_Ips_Is)*ttt_Is)),
+  f_Is_Ips=(1-f_E_Ips-f_Ips_Ips)*
+    exp(-(1/(1/q_E_Ips+1/q_Ips_Is+1/gamma_Is)*ttt_Ips)),
+  f_Is_Is=(1-f_E_Is-f_Ips_Is)*
+    exp(-(1/(1/q_E_Ips+1/q_Ips_Is+1/gamma_Is)*ttt_Is)),
+  eps_E_Ips=f_test*f_Ips*f_E_Ips, #traced infections while Ips to be removed
+  eps_E_Is=f_test*f_Is*f_E_Is, #traced infections while Is to be removed
+  eps_Ips_Ips=f_test*f_Ips*f_Ips_Ips, #traced infections while Ips to be removed
+  eps_Ips_Is=f_test*f_Is*f_Ips_Is,#traced infections while Ips to be removed
+  eps_Is_Ips=f_test*f_Ips*f_Is_Ips,#traced infections while Ips to be removed
+  eps_Is_Is=f_test*f_Is*f_Is_Is)#traced infections while Ips to be removed
+
+dat3=data.frame(Delay=testdelay,Ncont_p_inf=Ncont_p_inf,Ninf=rep(Ninf,length(tdelay)*length(Ncont_p_inf)),
+                contacts_per_tracer_calls=c(Ninf*Ncont_p_inf/(NCT*NCCTD)),R0v=R0a(pars1))
+cbp1 <- c("#000000","#999999", "#E69F00", "#56B4E9", "#009E73","#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+p1=ggplot(dat3, aes(y=R0v, x=contacts_per_tracer_calls,colour=factor(Delay))) + 
+  theme_bw()+geom_point(size=2)+coord_cartesian(ylim=c(0.4,3.05))+
+  scale_x_continuous(trans = 'log10')+
+  theme(axis.title=element_text(size=25),legend.position = "none",
+        axis.text=element_text(size=25))+
+  xlab(expression(Ratio~of~contacts~needing~tracing~to~tracing~capacity))+
+  ylab(expression("Reproductive number, "~R[t]))+
+  annotate(geom="text",x=0.25, y=aggregate(R0v~Delay,data=dat3,FUN=min)$R0v+.05,
+           label=unique(dat3$Delay),size=7)+
+  annotate(geom="text",x=0.5, y=.88*aggregate(R0v~Delay,data=dat3,FUN=min)$R0v[1],
+           label="Delay: symptoms->testing/tracing",size=7)+
+  theme(panel.grid.minor = element_blank())+scale_colour_manual(values=cbp1);p1
+#ggsave("Fig1.pdf",plot=p1,width=12,height=8)
+ggsave("Fig1.tiff",plot=p1,width=12,height=8,units="in",compression = "lzw")
+
+#Fig 2-------------------------------------------
+Ncont_p_inf= c(5,10,20,30) # contacts per infection - value before lockdowns 30
+#Ncont_p_inf= 10 # contacts per infection - value before lockdowns 30
+capcontratio=(NCT*NCCTD)/(rep(Ninf,length(Ncont_p_inf))*rep(Ncont_p_inf,each=length(Ninf)))
+capcontratio[capcontratio>1]=1
+tdelay=5;testdelay=rep(tdelay,each=length(Ninf)*length(Ncont_p_inf))
+
+pars2 = data.frame(N=100001,S=100001,
+                   beta = 0.35,
+                   kappa=1,
+                   sigma_Ips=presympInf/postsympInf,
+                   sigma_Is=1,
+                   gamma_Is=1/5,
+                   q_E_Ips=1/3.2,
+                   q_Ips_Is=1/2.3,
+                   q_Q_R=1/14,
+                   tau_Is=1/testdelay,
+                   ftraced=capcontratio,
+                   tracetime=0.5, #fixing avg time to trace to 1/2 day
+                   CTR=T,
+                   Ncont_p_inf= 10, # contacts per infection
+                   NCCTD=12, # # of contacts a contact tracer can contact per day to isolate; 1.5/hr
+                   NCT=15 # # of contact tracers for population
+)
+
+pars2 = pars2 %>% mutate (
+  alpha=gamma_Is*(0.0066/0.8)/(1-(0.0066/0.8)),
+  f_test=tau_Is/(tau_Is+gamma_Is),
+  f_Ips=(sigma_Ips/q_Ips_Is)/
+    (sigma_Ips/q_Ips_Is+sigma_Is/(gamma_Is+tau_Is)),
+  f_Is=(sigma_Is/(gamma_Is+tau_Is))/
+    (sigma_Ips/q_Ips_Is+sigma_Is/(gamma_Is+tau_Is)),
+  ttt_Ips=1/tau_Is+1/q_Ips_Is+tracetime, #time to trace infect caused by Ips
+  ttt_Is=1/tau_Is+tracetime, #time to trace infect caused by Ips
+  f_E_Ips=exp(-q_E_Ips*ttt_Ips), #fraction of traced infections caused while Ips still in Ea
+  f_E_Is=exp(-q_E_Ips*ttt_Is), #fraction of traced infections caused while Is still in Ea
+  f_Ips_Ips=(1-f_E_Ips)*exp(-(1/(1/q_E_Ips+1/q_Ips_Is)*ttt_Ips)),
+  f_Ips_Is=(1-f_E_Is)*exp(-(1/(1/q_E_Ips+1/q_Ips_Is)*ttt_Is)),
+  f_Is_Ips=(1-f_E_Ips-f_Ips_Ips)*
+    exp(-(1/(1/q_E_Ips+1/q_Ips_Is+1/gamma_Is)*ttt_Ips)),
+  f_Is_Is=(1-f_E_Is-f_Ips_Is)*
+    exp(-(1/(1/q_E_Ips+1/q_Ips_Is+1/gamma_Is)*ttt_Is)),
+  eps_E_Ips=f_test*f_Ips*f_E_Ips, #traced infections while Ips to be removed
+  eps_E_Is=f_test*f_Is*f_E_Is, #traced infections while Is to be removed
+  eps_Ips_Ips=f_test*f_Ips*f_Ips_Ips, #traced infections while Ips to be removed
+  eps_Ips_Is=f_test*f_Is*f_Ips_Is,#traced infections while Ips to be removed
+  eps_Is_Ips=f_test*f_Ips*f_Is_Ips,#traced infections while Ips to be removed
+  eps_Is_Is=f_test*f_Is*f_Is_Is)#traced infections while Ips to be removed
+
+dat4=data.frame(Delay=testdelay,Ncont_p_inf=rep(Ncont_p_inf,each=length(Ninf)),Ninf=rep(Ninf,length(Ncont_p_inf)*length(tdelay)),
+                cases_per_tracer_calls=c(rep(Ninf,length(Ncont_p_inf)*length(tdelay))/(NCT*NCCTD)))
+dat4$R0v=R0a(pars2)
+cbp2 <- c("#F0E442","#009E73","#0072B2", "#D55E00", "#CC79A7")
+p2=ggplot(dat4, aes(y=R0v, x=cases_per_tracer_calls,color=factor(Ncont_p_inf))) + 
+  theme_bw()+geom_point(size=2)+coord_cartesian(ylim=c(0.4,3.05))+
+  scale_x_continuous(trans = 'log10')+
+  theme(axis.title=element_text(size=25),legend.position = "none",
+        axis.text=element_text(size=25))+
+  xlab(expression(Cases~per~contact~tracer~calls~per~day))+
+  ylab(expression("Reproductive number, "~R[t]))+
+  annotate(geom="text",x=c(10^(c(-.4,-.7,-1,-1.2)-.35)), y=1.6,
+           label=Ncont_p_inf,size=7)+
+  annotate(geom="text",x=.09, y=1.4,
+           label="Contacts/case",size=7)+
+  theme(panel.grid.minor = element_blank())+scale_colour_manual(values=cbp2);p2
+#ggsave("Fig2.pdf",plot=p2,width=12,height=8)
+ggsave("Fig2.tiff",plot=p2,width=12,height=8,units="in",compression = "lzw")
+
+#-----------------------Sensitivity analysis Fig S3
+Ninf= 18# # of cases/infections traced
+Ncont_p_inf=10 # contacts per infection - value before lockdowns 30
+NCCTD=12 # # of contacts a contact tracer can contact per day to isolate; 1.5/hr
+NCT=15 # # of contact tracers for population - CA target
+capcontratio=(NCT*NCCTD)/(Ninf*Ncont_p_inf);capcontratio[capcontratio>1]=1
+pars5 = data.frame(
+                   kappa=1,
+                   beta = 0.35,
+                   sigma_Ips=presympInf/postsympInf,
+                   sigma_Is=1,
+                   gamma_Is=1/5,
+                   q_E_Ips=1/3.2,
+                   q_Ips_Is=1/2.3,
+                   tau_Is=1/5,
+                   alpha=0.0025)
+
+Rt_sens=data.frame(matrix(NA,nrow=length(pars5),ncol=2));colnames(Rt_sens)=c("dec","inc");rownames(Rt_sens)=names(pars5)
+for (k in 1:length(pars5)) {
+  parsR0_sens=pars5
+  for (l in 1:2) {
+    parsR0_sens[k]=pars5[k]*(.7+0.2*l)
+    pars6 = parsR0_sens %>% mutate (N=100001,S=100001,
+                              q_Q_R=1/14,
+                              ftraced=capcontratio,
+                              tracetime=0.5,
+                              CTR=T,
+                              Ncont_p_inf= 10, # contacts per infection
+                              NCCTD=12, # # of contacts a contact tracer can contact per day to isolate; 1.5/hr
+                              NCT=15, # # of contact tracers for population
+                              f_test=tau_Is/(tau_Is+gamma_Is),
+                              f_Ips=(sigma_Ips/q_Ips_Is)/
+                                (sigma_Ips/q_Ips_Is+sigma_Is/(gamma_Is+tau_Is)),
+                              f_Is=(sigma_Is/(gamma_Is+tau_Is))/
+                                (sigma_Ips/q_Ips_Is+sigma_Is/(gamma_Is+tau_Is)),
+                              ttt_Ips=1/tau_Is+1/q_Ips_Is+tracetime, #time to trace infect caused by Ips
+                              ttt_Is=1/tau_Is+tracetime, #time to trace infect caused by Ips
+                              f_E_Ips=exp(-q_E_Ips*ttt_Ips), #fraction of traced infections caused while Ips still in Ea
+                              f_E_Is=exp(-q_E_Ips*ttt_Is), #fraction of traced infections caused while Is still in Ea
+                              f_Ips_Ips=(1-f_E_Ips)*exp(-(1/(1/q_E_Ips+1/q_Ips_Is)*ttt_Ips)),
+                              f_Ips_Is=(1-f_E_Is)*exp(-(1/(1/q_E_Ips+1/q_Ips_Is)*ttt_Is)),
+                              f_Is_Ips=(1-f_E_Ips-f_Ips_Ips)*
+                                exp(-(1/(1/q_E_Ips+1/q_Ips_Is+1/gamma_Is)*ttt_Ips)),
+                              f_Is_Is=(1-f_E_Is-f_Ips_Is)*
+                                exp(-(1/(1/q_E_Ips+1/q_Ips_Is+1/gamma_Is)*ttt_Is)),
+                              eps_E_Ips=f_test*f_Ips*f_E_Ips, #traced infections while Ips to be removed
+                              eps_E_Is=f_test*f_Is*f_E_Is, #traced infections while Is to be removed
+                              eps_Ips_Ips=f_test*f_Ips*f_Ips_Ips, #traced infections while Ips to be removed
+                              eps_Ips_Is=f_test*f_Is*f_Ips_Is,#traced infections while Ips to be removed
+                              eps_Is_Ips=f_test*f_Ips*f_Is_Ips,#traced infections while Ips to be removed
+                              eps_Is_Is=f_test*f_Is*f_Is_Is)#traced infections while Ips to be removed
+    
+    Rt_sens[k,l]=R0a(pars6)   
+}}
+
+parlabs=c(expression(kappa),
+          expression(beta),
+          expression(sigma[Ips]),
+          expression(sigma[Is]),
+          expression(gamma[Is]),
+          expression(q[E-Ips]),
+          expression(q[Ips-Is]),
+          expression(tau[Is]),
+          expression(alpha[Is])
+)
+Rtbase=sum(Rt_sens[1,])/2
+Rt_sensR=(Rt_sens-Rtbase)/Rtbase
+#pdf("S3_Fig.pdf",width=10,height=8)
+tiff("S3_Fig.tif",width=10,height=8,units="in",res=300,compression = "lzw")
+par(mar=c(4, 3, .5, 0.5),mgp=c(2.5,.5,0),cex=2)
+barplot(Rt_sensR[,1],horiz=T,names.arg=parlabs,las=2,col="blue",xlim=c(-.11,.11),#xlab="Change in R0")
+        xlab=expression(Change~"in"~"reproductive number,"~R[t]),las=1)
+barplot(Rt_sensR[,2],horiz=T,names.arg=parlabs,las=2,col="red",add=T,las=1)
+abline(h=c(.75,1.85,3.05,4.25,5.45,6.65,7.85,9.05,10.25),col="grey",lty=3)
+box();#grid(ny=nrow(Rt_sens)+1)
+dev.off()
---
+++ Fig 3 Final.R
@@ -0,0 +1,137 @@
+setwd("c:/marm/research/covid-19/modeling T-CT-I/")
+require(deSolve);require(tidyverse)
+R0a = function(parsR) { #function for R0 expression
+  with(parsR,
+       kappa*beta*(S/N)*(sigma_Is*q_Ips_Is/(alpha_Is+gamma_Is+tau_Is+eps_Is_Ips*ftraced+eps_Is_Is*ftraced)*(q_E_Ips/(q_Ips_Is+eps_Ips_Ips*ftraced+eps_Ips_Is*ftraced))
+                         +(sigma_Ips*q_E_Ips/(q_Ips_Is+eps_Ips_Ips*ftraced+eps_Ips_Is*ftraced)))*(1/(q_E_Ips+eps_E_Ips*ftraced+eps_E_Is*ftraced))) }
+
+SIRb=function(Time, State, Pars) {
+  with(as.list(c(State, Pars)), {    
+    capcontratio=(NCT*NCCTD)/(Is*tau_Is*Ncont_p_inf);capcontratio[capcontratio>1]=1
+    ftraced=ifelse(CTR,capcontratio,0)
+    
+    N=S+E+Ips+Is+R+Q
+    dS=-kappa*beta*S*(sigma_Ips*Ips+sigma_Is*Is)/N
+    dE=kappa*beta*S*(sigma_Ips*Ips+sigma_Is*Is)/N-q_E_Ips*E-eps_E_Ips*ftraced*E-eps_E_Is*ftraced*E
+    dIps=q_E_Ips*E-q_Ips_Is*Ips-eps_Ips_Ips*ftraced*Ips-eps_Ips_Is*ftraced*Ips
+    dIs=q_Ips_Is*Ips-gamma_Is*Is-tau_Is*Is-eps_Is_Ips*ftraced*Is-eps_Is_Is*ftraced*Is-alpha_Is*Is
+    dQ=(eps_E_Ips*ftraced+eps_E_Is*ftraced)*E+(eps_Ips_Ips*ftraced+eps_Ips_Is*ftraced)*Ips+
+      (eps_Is_Ips*ftraced+eps_Is_Is*ftraced+tau_Is)*Is-q_Q_R*Q-alpha_Q*Q
+    dR=gamma_Is*Is+q_Q_R*Q
+    
+    return(list(c(dS,dE,dIps,dIs,dQ,dR)))
+  })
+}
+
+#params
+#Infectiousness vs day from He et al gamma distribution parameterized w/ mean, variance
+shape=function(mu,sig) mu^2/sig^2;scale = function(mu,sig) sig^2/mu;mn=3.1;sig2=2.15
+presympInf=pgamma(2.3,shape=shape(mn,sig2),scale=scale(mn,sig2))/2.3 #relative daily infectiousness pre-symptomatic
+postsympInf=(pgamma(7.3,shape=shape(mn,sig2),scale=scale(mn,sig2))-pgamma(2.3,shape=shape(mn,sig2),scale=scale(mn,sig2)))/5 #relative daily infectiousness mildly-symptomatic
+ssInf=(pgamma(13.3,shape=shape(mn,sig2),scale=scale(mn,sig2))-pgamma(7.3,shape=shape(mn,sig2),scale=scale(mn,sig2)))/6 #relative daily infectiousness severely-symptomatic
+
+pars3 = data.frame(N=100001,
+                   beta = 0.35,
+                   kappa=1,
+                   sigma_Ips=presympInf/postsympInf,
+                   sigma_Is=1,
+                   gamma_Is=1/5,
+                   q_E_Ips=1/3.2,
+                   q_Ips_Is=1/2.3,
+                   q_Q_R=1/14,
+                   tau_Is=1/5,
+                   tracetime=0.5,
+                   CTR=T,
+                   Ncont_p_inf= 10, # contacts per infection
+                   NCCTD=12, # # of contacts a contact tracer can contact per day to isolate; 1.5/hr
+                   NCT=15 # # of contact tracers for population
+)
+
+pars3 = pars3 %>% mutate (
+  alpha_Is=gamma_Is*(0.0066/0.8)/(1-(0.0066/0.8)),
+  alpha_Q=q_Q_R*(0.0066/0.8)/(1-(0.0066/0.8)),
+  f_test=tau_Is/(tau_Is+gamma_Is),
+  f_Ips=(sigma_Ips/q_Ips_Is)/
+    (sigma_Ips/q_Ips_Is+sigma_Is/(gamma_Is+tau_Is)),
+  f_Is=(sigma_Is/(gamma_Is+tau_Is))/
+    (sigma_Ips/q_Ips_Is+sigma_Is/(gamma_Is+tau_Is)),
+  ttt_Ips=1/tau_Is+1/q_Ips_Is+tracetime, #time to trace infect caused by Ips
+  ttt_Is=1/tau_Is+tracetime, #time to trace infect caused by Ips
+  f_E_Ips=exp(-q_E_Ips*ttt_Ips), #fraction of traced infections caused while Ips still in Ea
+  f_E_Is=exp(-q_E_Ips*ttt_Is), #fraction of traced infections caused while Is still in Ea
+  f_Ips_Ips=(1-f_E_Ips)*exp(-(1/(1/q_E_Ips+1/q_Ips_Is)*ttt_Ips)),
+  f_Ips_Is=(1-f_E_Is)*exp(-(1/(1/q_E_Ips+1/q_Ips_Is)*ttt_Is)),
+  f_Is_Ips=(1-f_E_Ips-f_Ips_Ips)*
+    exp(-(1/(1/q_E_Ips+1/q_Ips_Is+1/gamma_Is)*ttt_Ips)),
+  f_Is_Is=(1-f_E_Is-f_Ips_Is)*
+    exp(-(1/(1/q_E_Ips+1/q_Ips_Is+1/gamma_Is)*ttt_Is)),
+  eps_E_Ips=f_test*f_Ips*f_E_Ips, #traced infections while Ips to be removed
+  eps_E_Is=f_test*f_Is*f_E_Is, #traced infections while Is to be removed
+  eps_Ips_Ips=f_test*f_Ips*f_Ips_Ips, #traced infections while Ips to be removed
+  eps_Ips_Is=f_test*f_Is*f_Ips_Is,#traced infections while Ips to be removed
+  eps_Is_Ips=f_test*f_Ips*f_Is_Ips,#traced infections while Ips to be removed
+  eps_Is_Is=f_test*f_Is*f_Is_Is)#traced infections while Ips to be removed
+
+times=seq(0, 350, by = .5);yini=c(S=100000,E=0,Ips=5,Is=5,Q=0,R=0)
+options(scipen=5);titlocH=240;titlocV1=99200;titlocV2=93200
+
+#Social distancing panels A,B
+parsSD= pars3 %>% mutate (kappa=0.5799608, CTR=F)
+#parsSD= pars3 %>% mutate (kappa=1,tau_Is=0, CTR=F) #run to get R0 w/ no testing or tracing 3.2
+
+tiff("Fig 3.tif",width=14,height=8,units="in",res=300,compression = "lzw")
+#pdf("Fig 3.pdf",width=14,height=8)
+dynSD=as.data.frame(ode(yini, times, SIRb, parsSD));dynSD$I=dynSD$Ips+dynSD$Is+dynSD$E
+cl1=c(3,2,5);ctp=c("S","I","R") #cl1=c(3,1,2,7,4,5);
+par(mfcol=c(2,3),mar=c(0, 3, 0, 0),mgp=c(1.5,.5,0),oma = c(.5, .5, 0.5, 0.5),cex=1.25)
+matplot(x=dynSD[,1],y=dynSD[,ctp],type="l",xaxt="n",lty=1,lwd=2,ylim=c(0,103000),
+        xlab="Day",ylab="Number of people",col=cl1)
+#legend(20,.8e5,c("S","E",expression(I[ps]),expression(I[s]),"Q","R"),col=cl1,lty=1,bty="n",seg.len=1,lwd=2,x.intersp=.5)
+legend(5,.8e5,c("S",expression(E+I[s]+I[ps]),"R"),col=cl1,lty=1,bty="n",seg.len=1,lwd=2,x.intersp=.5)
+text(0,95000,"A")
+text(titlocH+5,titlocV1,"Social distancing")
+text(titlocH+5,titlocV2,"No contact tracing")
+
+parsSD2=data.frame(dynSD, parsSD)
+parsSD2$ftraced=0
+R0sd=R0a(parsSD2);
+par(mar=c(3, 3, 0, 0) )#b,l,t,r
+plot(R0sd~dynSD[,1],xlab="",ylab=expression("Reproductive number, "~R[t]),type="l",ylim=c(0,2.0),col=8,lwd=2) #Rt over time
+text(0,2.0,"D")
+
+parsUCT= pars3 %>% mutate (kappa=1,CTR=T,NCT=1500)
+dynUCT=as.data.frame(ode(yini, times, SIRb, parsUCT));dynUCT$I=dynUCT$Ips+dynUCT$Is+dynUCT$E
+par(mar=c(0, 0, 0, 0),mgp=c(1.5,.5,0),cex=1.25)
+matplot(x=dynUCT[,1],y=dynSD[,ctp],col=cl1,type="l",xaxt="n",yaxt="n",lty=1,lwd=2,ylim=c(0,103000),
+        xlab="Day",ylab="Number of people")
+parsUCT=data.frame(dynUCT, parsUCT)
+parsUCT$capcontratio=(parsUCT$NCT*parsUCT$NCCTD)/(parsUCT$Is*parsUCT$tau_Is*parsUCT$Ncont_p_inf)
+parsUCT$ftraced=parsUCT$capcontratio;parsUCT$ftraced[parsUCT$ftraced>1]=1
+R0uct=R0a(parsUCT)
+text(0,95000,"B")
+text(titlocH,titlocV1,"No social distancing")
+text(titlocH,titlocV2,"Eff. unlimited contact tracing")
+par(mar=c(3, 0, 0, 0) )#b,l,t,r
+plot(R0uct~dynUCT[,1],xlab="Day",cex.lab=1.2,type="l",yaxt="n",ylim=c(0,2.0),col=8,lwd=2) #Rt over time
+text(0,2.0,"E")
+
+parsLCT=pars3 %>% mutate(kappa=1,NCT=15) 
+dynLCT=as.data.frame(ode(yini, times, SIRb, parsLCT));dynLCT$I=dynLCT$Ips+dynLCT$Is+dynLCT$E
+par(mar=c(0, 0, 0, 0),mgp=c(1.5,.5,0),cex=1.25)
+matplot(x=dynLCT[,1],y=dynLCT[,ctp],col=cl1,type="l",xaxt="n",yaxt="n",lty=1,lwd=2,ylim=c(0,103000),
+        xlab="Day",ylab="Number of people")
+text(0,95000,"C")
+text(titlocH,titlocV1,"No social distancing")
+text(titlocH,titlocV2,"Limited contact tracing")
+
+parsLCT2=data.frame(dynLCT, parsLCT)
+parsLCT2$capcontratio=(parsLCT2$NCT*parsLCT2$NCCTD)/(parsLCT2$Is*parsLCT2$tau_Is*parsLCT2$Ncont_p_inf)
+parsLCT2$ftraced=replace(parsLCT2$capcontratio, parsLCT2$capcontratio>1,1)
+R0lct=R0a(parsLCT2)
+par(mar=c(3, 0, 0, 0) )#b,l,t,r
+plot(R0lct~dynLCT[,1],xlab="",type="l",yaxt="n",ylim=c(0,2.0),col=8,lwd=2) #Rt over time
+text(0,2.0,"F")
+dev.off()
+#----------------------
+R0uct[1]/R0sd[1] #ratio of starting Rt values
+R0lct[1]/R0sd[1] #ratio of starting Rt values
